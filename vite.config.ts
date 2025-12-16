e1f50296969ff5efb637f082b685a6206ad50381
haqki32-collab <haqki32@gmail.com>
2025-12-17 03:18:45 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // This line is essential for Capacitor/Android to load assets from local file system
   base: './', 
   build: {
     outDir: 'dist',
